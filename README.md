4f798dbca788b9d158addf66ef97117a10970dba
Fabian Brash <fabianbrash@gmail.com>
2020-10-18 17:00:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Working with and manipulating Javascript DOM
+
+##
+
+`this`
+### Note on the function that uses the 'this' keyword I used a standard func instead of an array function, arrow functions treat 'this'
+### differently, I nee to learn how to fix that.
---
+++ app.js
@@ -0,0 +1,39 @@
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
+const addListBtn = document.querySelector(".addListBtn");
+
+//text.style.color = "red";
+//text.style.backgroundColor = "red"; 
+//text.classList.add("change");
+
+changeColor.addEventListener("click", () => {
+    //text.classList.add('change');
+    text.classList.toggle('change');
+});
+
+
+const userList = document.querySelectorAll(".name-list li");
+
+const userListRoot = document.querySelector(".name-List");
+const listInput = document.querySelector(".list-input");
+
+console.log(userList);
+
+for(user of userList){
+    user.addEventListener('click', function() {
+        console.log(this);
+        this.style.color = "red";
+    });
+}
+
+addListBtn.addEventListener('click', () => {
+    //Create a li out of thin air
+      const newLi = document.createElement('LI');
+      const liContent = document.createTextNode(listInput.value);
+      console.log(listInput.value);
+    //Add the input value inside that new li
+    newLi.appendChild(liContent);
+
+    //Attach the li to the user List
+    userListRoot.appendChild(newLi);
+})
